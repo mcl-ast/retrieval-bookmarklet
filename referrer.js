46f422bbb9b12f0cadd65973b6891031398b7c4f
Joel Kraft <joelkraft3@yahoo.com>
2013-12-16 13:03:45 -0800

    MyMCL page scrape only. Also support for localStorage of script.

---
+++ referrer.js
@@ -0,0 +1,30 @@
+(function() {
+	var url = 'http://localhost/~joelkraft/Jobs/Multco/retrieval_upgrade_nov-2013/bookmarklet.js',
+		noCache = '?nc=' + (parseInt(Math.random() * 100000000000)).toString();
+
+	var jsCode = document.createElement('script');	
+	jsCode.setAttribute('src', url + noCache);
+	jsCode.setAttribute('type', 'text\/javascript');
+	jsCode.onerror = loadError;
+	document.head.appendChild(jsCode);
+	
+	function loadError (oError) {
+  		console.log('Remote Script could not be loaded.');
+		if (window.localStorage && window.localStorage.retrievalSelectionScript) {
+	  		var codeText = JSON.parse(window.localStorage.retrievalSelectionScript).script,
+				jsCode = document.createElement('script');	
+	  		jsCode.setAttribute('type', 'text\/javascript');
+	  		jsCode.text = codeText;
+	  		document.body.appendChild(jsCode);
+		} else {
+			alert('This function is not available at this time.  Please ensure you are on a Catalog page, and try again.');
+			console.log('window.localStorage: ' + !!window.localStorage + '\nwindow.localStorage.retrievalSelectionScript: ' + !!window.localStorage.retrievalSelectionScript);
+			throw new URIError("The script " + oError.target.src + " is not accessible.");
+		}
+		return true;
+	}
+})();
+
+// keep minified version current with the above.  www.jscompress.com
+// comment the minified script when it needs updating
+javascript:(function(){function r(e){console.log("Remote Script could not be loaded.");if(window.localStorage&&window.localStorage.retrievalSelectionScript){var t=JSON.parse(window.localStorage.retrievalSelectionScript).script,n=document.createElement("script");n.setAttribute("type","text/javascript");n.text=t;document.body.appendChild(n)}else{alert("This function is not available at this time.  Please ensure you are on a Catalog page, and try again.");console.log("window.localStorage: "+!!window.localStorage+"\nwindow.localStorage.retrievalSelectionScript: "+!!window.localStorage.retrievalSelectionScript);throw new URIError("The script "+e.target.src+" is not accessible.")}return true}var e="http://localhost/~joelkraft/Jobs/Multco/retrieval_upgrade_nov-2013/bookmarklet.js",t="?nc="+parseInt(Math.random()*1e11).toString();var n=document.createElement("script");n.setAttribute("src",e+t);n.setAttribute("type","text/javascript");n.onerror=r;document.head.appendChild(n)})()
