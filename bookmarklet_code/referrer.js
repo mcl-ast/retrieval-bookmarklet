cec8f7c056da370514eb96324909cbd20fbd9d12
Joel Kraft <joelkraft3@yahoo.com>
2014-02-18 14:11:23 -0800

    Further cleanup after merging in MyMCLi

--- bookmarklet_code/referrer.js
+++ bookmarklet_code/referrer.js
@@ -25,15 +25,7 @@
 	}
 })();
 
-<<<<<<< HEAD:referrer.js
 // Actual bookmarklet code (minified version of above):
-=======
-// Actual bookmarklet code:
->>>>>>> MyMCLi:BookmarkletCode/referrer.js
 // keep minified version current with the above.  www.jscompress.com
 // comment the minified script when it needs updating
 javascript:(function(){function r(e){console.log("Remote Script could not be loaded.");if(window.localStorage&&window.localStorage.retrievalSelectionScript){var t=JSON.parse(window.localStorage.retrievalSelectionScript).script,n=document.createElement("script");n.setAttribute("type","text/javascript");n.text=t;document.body.appendChild(n)}else{alert("This function is not available at this time.  Please ensure you are on a Catalog page, and try again.");console.log("window.localStorage: "+!!window.localStorage+"\nwindow.localStorage.retrievalSelectionScript: "+!!window.localStorage.retrievalSelectionScript);throw new URIError("The script "+e.target.src+" is not accessible.")}return true}var e="http://localhost/~joelkraft/Jobs/Multco/retrieval_upgrade_nov-2013/bookmarklet.js",t="?nc="+parseInt(Math.random()*1e11).toString();var n=document.createElement("script");n.setAttribute("src",e+t);n.setAttribute("type","text/javascript");n.onerror=r;document.head.appendChild(n)})()
-
-// And here's a version at joelkraft.net:
-
-javascript:(function(){function r(e){console.log("Remote Script could not be loaded.");if(window.localStorage&&window.localStorage.retrievalSelectionScript){var t=JSON.parse(window.localStorage.retrievalSelectionScript).script,n=document.createElement("script");n.setAttribute("type","text/javascript");n.text=t;document.body.appendChild(n)}else{alert("This function is not available at this time.  Please ensure you are on a Catalog page, and try again.");console.log("window.localStorage: "+!!window.localStorage+"\nwindow.localStorage.retrievalSelectionScript: "+!!window.localStorage.retrievalSelectionScript);throw new URIError("The script "+e.target.src+" is not accessible.")}return true}var e="http://www.joelkraft.net/ideas/exp/remote.js",t="?nc="+parseInt(Math.random()*1e11).toString();var n=document.createElement("script");n.setAttribute("src",e+t);n.setAttribute("type","text/javascript");n.onerror=r;document.head.appendChild(n)})()
